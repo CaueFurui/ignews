af5342d4d0d12f7b3dd30446050bc57a0cbccd12
Martins, Caue (154-Extern) <caumart@americas.corpdir.net>
2022-03-22 11:50:49 -0300

    configuring stripe and ssr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,19 @@
 import Head from 'next/head'
+import { GetServerSideProps } from 'next'
+import Avatar from '../../public/images/avatar.svg'
 import { SubscribeButton } from '../components/SubscribeButton'
 import styles from './home.module.scss'
+import { stripe } from '../services/stripe'
+import Image from 'next/image'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string
+    amount: string
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -17,13 +28,31 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $ 9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
-        <img src="images/avatar.svg" alt="Girl coding" />
+        <Image src={Avatar} alt="Girl coding" />
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1Kg8TKDi4Fqp03883ef2JO1B')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format((price.unit_amount / 100)),
+  }
+  
+  return {
+    props: {
+      product
+    }
+  }
 }
